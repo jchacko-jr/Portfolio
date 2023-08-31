da46a8919102755bc00543dcaf1cf7305a26fadd
Jomon Chacko <jomunchacko@gmail.com>
2023-08-31 14:48:17 +0530

    Php Console Update

---
+++ app/components/work/projectDetails.ts
@@ -0,0 +1,74 @@
+import {
+    SiCplusplus,
+    SiFramer,SiGoogledrive,
+    SiGithub, SiNeovim,
+    SiNextdotjs,
+    SiReact,
+    SiRust,
+    SiTailwindcss,
+    SiTypescript,
+    SiZig,
+    SiAdobeaftereffects,
+    SiAdobephotoshop,
+    SiCinema4D,
+    SiFigma, 
+    SiUnrealengine,
+    SiAdobepremierepro,
+} from "react-icons/si";
+import {IconType} from "react-icons";
+
+export type ProjectProps = {
+  id: number;
+  name: string;
+  description: string;
+  technologies: IconType[];
+  techNames: string[];
+  techLinks: string[];
+  github: string;
+  drive:string;
+  demo: string;
+  image: string;
+  available: boolean;
+};
+
+export const projects = [
+    {
+        id: 0,
+        name: "Yip 5.0 Promo",
+        description:
+            "YIP 5.0 Promo,crafted with the help of software After Effects 2022.The promo captures the essence of YIP, highlighting its key features and the incredible opportunities. ",
+        technologies: [SiAdobeaftereffects, SiAdobephotoshop,SiCinema4D,SiFigma,SiAdobepremierepro],
+        techNames: ["After Effects", "Photoshop", "Cinema4D", "Figma","Premire Pro"],
+        techLinks: ["https://www.adobe.com/products/aftereffects.html", "https://www.adobe.com/products/photoshop.html", "https://www.maxon.net/en/cinema-4d", "https://www.figma.com/" ,"https://www.adobe.com/ro/products/premiere.html"],
+        github: "https://github.com/jchacko-jr/Yip-5.0-Video",
+        demo: "https://drive.google.com/file/d/1LfRsjF_zO9-WlJz40lj9AKAFxojff9JC/view?usp=drive_link",
+        image: "/projects/port.png",
+        available: true,
+    },
+    {
+        id: 1,
+        name: "Beyoud Us Promo",
+        description:
+            "Official promo of Beyoud Us Hackathon Hosted by GTECH μLearn & Kerala Knowledge Economy Mission (KKEM) ",
+        technologies:[SiCinema4D,SiFigma,SiAdobepremierepro],
+        techNames: ["Cinema4D", "Figma","Premire Pro"],
+        techLinks: ["https://www.maxon.net/en/cinema-4d", "https://www.figma.com/" ,"https://www.adobe.com/ro/products/premiere.html"],
+        github: "https://www.instagram.com/reel/CuE0CO0Arp1/",
+        demo: "https://www.instagram.com/reel/CuE0CO0Arp1/",
+        image: "/projects/2.jpg",
+        available: true,
+    },
+    {
+        id: 2,
+        name: "Coming Soon",
+        description:
+            " I'm currently working on a couple of projects. I'll update this section as soon as I'm done.",
+        technologies: [SiUnrealengine ,SiAdobeaftereffects],
+        techNames: ["Unreal Engine 5","AfterEffects"],
+        techLinks: ["https://www.unrealengine.com/" ,"https://www.adobe.com/products/aftereffects.html"],
+        github: "https://github.com/jchacko-jr/Yip-5.0-Video",
+        demo: "https://www.unrealengine.com/",
+        image: "/projects/construction.webp",
+        available: false,
+    },
+];
---
+++ app/layout.tsx
@@ -0,0 +1,100 @@
+import "./globals.css";
+import React, { ReactNode } from "react";
+import type { Metadata } from "next";
+import { Syne } from "next/font/google";
+
+const syne = Syne({
+    subsets: ["latin"],
+    display: "block",
+    weight: ["400", "500", "600", "700", "800"],
+});
+
+export const metadata: Metadata = {
+    metadataBase: new URL("https://www.seekvisualartist.com/"),
+    title: "Jomon Chacko Portfolio",
+    description:
+    "My name is Jomon Chacko and I am a 3D animation artist, a proficient user of Unreal Engine 5, and skilled in video editing and graphic design. With years of experience in these fields, I have honed my skills and am excited to showcase my work to you. I am passionate about visually stunning and engaging content that captivates audiences.",
+    generator: "Next.js",
+    applicationName: "Portfilio Joe",
+    keywords: [
+        "freelance",
+        "developer",
+        "freelance developer",
+        "frontend",
+        "nextjs",
+        "astro",
+        "react",
+        "frontend developer",
+        "frontend engineer",
+        "creative",
+        "creative developer",
+        "creative engineer",
+        "tech",
+        "germany",
+        "software",
+        "software developer",
+        "portfolio",
+        "frontend developer portfolio",
+        "creative developer portfolio",
+        "creative engineer portfolio",
+        "software developer portfolio",
+        "frontend engineer portfolio",
+    ],
+    colorScheme: "dark",
+    openGraph: {
+        title: "Seek Visual Artist - Designer and Developer",
+        description:
+      "My name is Jomon Chacko and I am a 3D animation artist, a proficient user of Unreal Engine 5, and skilled in video editing and graphic design. With years of experience in these fields, I have honed my skills and am excited to showcase my work to you. I am passionate about visually stunning and engaging content that captivates audiences..",
+        url: "",
+        siteName: "",
+        images: [
+            {
+                url: "",
+                width: 1200,
+                height: 630,
+                alt: "",
+            },
+        ],
+        locale: "en-US",
+        type: "website",
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Jomon Chacko Portfilio",
+        description:
+      "My name is Jomon Chacko and I am a 3D animation artist, a proficient user of Unreal Engine 5, and skilled in video editing and graphic design. With years of experience in these fields, I have honed my skills and am excited to showcase my work to you. I am passionate about visually stunning and engaging content that captivates audiences.",
+        creator: "SeekVFX",
+        creatorId: "0000000000",
+        images: ["./public/metadata.jpg"],
+    },
+    robots: {
+        index: true,
+        follow: true,
+        nocache: false,
+        googleBot: {
+            index: true,
+            follow: false,
+            noimageindex: true,
+            "max-video-preview": -1,
+            "max-image-preview": "large",
+            "max-snippet": -1,
+        },
+    },
+    category: "technology",
+};
+
+type RootLayoutProps = {
+  children: ReactNode;
+};
+
+export default function RootLayout({ children }: RootLayoutProps) {
+    return (
+        <html lang="en">
+            <body
+                className={`${syne.className} scroll-smooth scrollbar-none scrollbar-track-[#0E1016] scrollbar-thumb-[#212531]`}
+            >
+                {children}
+            </body>
+        </html>
+    );
+}
